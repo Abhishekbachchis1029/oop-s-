c3e220448d7b9bd5b62b6897b41337dd3a547532
Abhishekbachchis1029 <abhishekbachchis2781@gmail.com>
2024-08-16 21:01:21 +0530

    this is the second assignment

---
+++ assignment 2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<stdlib.h>
+#include<stdio.h>
+using namespace std;
+struct address{
+    int house_no;
+    string area;
+};
+class DataBase{
+    private:
+        string name;
+        int roll_no;
+        string Class;
+        string Division;
+        int DOB;
+        string Blood_group;
+        long long contact;
+        address Address;
+        int driving_licence;
+    public:
+        DataBase(){
+
+        }
+        
+};
